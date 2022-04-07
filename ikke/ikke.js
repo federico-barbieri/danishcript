0f86a6135e48f3f6e3f83fcd3c6dda90653b2b48
Federico Barbieri <federicobarbieri54@gmail.com>
2022-04-07 12:34:09 +0200

    Add files via upload

---
+++ ikke/ikke.js
@@ -0,0 +1,85 @@
+// create an array with all the sentences 
+
+let sentences = ["Du kommer fra Tyskland,", "Marie taler ikke dansk,",
+    "Du cykler ikke på arbejde,",
+    "Du arbejder altid om lørdagen,",
+    "Han underviser  i engelsk,",
+    "Du er ikke 30 endnu,",
+    "Han er single,",
+    "Norreport ligger ikke på Østerbro,",
+    "Du har ikke fødselsdag på fredag,",
+    "Du har tre hunde,"]
+
+
+// store the keyword "ikke" in a variable
+
+let ikke = "ikke";
+
+
+// get a random number to use in the array
+
+const pickSentence = () => {
+    let storeIt = Math.floor(Math.random() * sentences.length);
+    return storeIt;
+  }
+
+// target span and modify it  
+
+const spanToModify = document.querySelector('.sentence-to-modify');
+
+// store the random number 
+
+let chosenNum = pickSentence();
+
+spanToModify.innerHTML = sentences[chosenNum];
+
+
+
+// create a function so after clicking on the buttons 
+// the whole process starts again
+
+function nextSentence(){
+    let chosenNum = pickSentence();
+    spanToModify.innerHTML = sentences[chosenNum];
+    ikkeBtn.style.border = "none";
+    velBtn.style.border = "none";
+}
+
+// target ikke and vel buttons 
+
+const ikkeBtn = document.querySelector('.ikke-btn');
+const velBtn = document.querySelector('.vel-btn');
+
+// event listeners for the buttons 
+
+ikkeBtn.addEventListener('click', () => {
+
+
+    // if you click on the ikke btn and you're correct, the border turns green
+    // Otherwise, it turns red
+
+    if (sentences[chosenNum].includes(ikke)){
+        ikkeBtn.style.border = "1px solid red";
+        setTimeout(nextSentence, 2000);
+    } else{
+        ikkeBtn.style.border = "1px solid green";
+        setTimeout(nextSentence, 2000);
+    }
+   
+})
+
+velBtn.addEventListener('click', () => {
+
+
+    // if you click on the vel btn and you're correct, the border turns green
+    // Otherwise, it turns red
+
+    if (sentences[chosenNum].includes(ikke)){
+        velBtn.style.border = "1px solid green";
+        setTimeout(nextSentence, 2000);
+    } else{
+        velBtn.style.border = "1px solid red";
+        setTimeout(nextSentence, 2000);
+    }   
+})
+
